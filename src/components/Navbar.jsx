e8375fa6d184230874e70da2ac9e389c86518212
Hashan <hemachandra.hashan@gmail.com>
2023-05-20 17:38:46 +0530

    Navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import {useState} from 'react';
+import {navLinks} from '../constants'
+
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <ul className='font-thirsty font-medium text-dimWhite cursor-pointer text-[25px]'>
+        Hashan M. Hemachadra
+      </ul>
+
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>{navLinks.map((nav, index) =>(
+        <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-dimWhite mr-10`}>
+          <a href={`#${nav.id}`}>{nav.title}</a>
+        </li>
+      ))}</ul>
+    </nav>
   )
 }
 
