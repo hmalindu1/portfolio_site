8d4026a50be09cb2a1fdde65aa0a5ec19ff5ff08
Hashan <hemachandra.hashan@gmail.com>
2023-05-20 18:34:53 +0530

    Color changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,14 @@ export const mode = "jit";
 export const theme = {
   extend: {
     colors: {
-      primary: "#00040f",
+      primary: "#1f2028",
       secondary: "#00f6ff",
       dimWhite: "rgba(255, 255, 255, 0.7)",
       dimBlue: "rgba(9, 151, 124, 0.1)",
     },
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
-      thirsty: ["Thirsty", "cursive"]
+      thirsty: ["Thirsty", "cursive"],
     },
   },
   screens: {
