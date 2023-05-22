b2f740d8f195886c107097bbc6dfd5ba0578cd0f
Hashan <hemachandra.hashan@gmail.com>
2023-05-23 00:39:02 +0530

    Created the mobile responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,39 @@
-import {useState} from 'react';
-import {navLinks} from '../constants'
-
-
+import { useState } from "react";
+import { navLinks } from "../constants";
+import { menu, close } from "../assets";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <ul className='font-thirsty font-medium text-white cursor-pointer text-[25px]'>
+      <ul className="font-thirsty font-medium text-white cursor-pointer text-[25px]">
         Hashan M. Hemachandra
       </ul>
 
-      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>{navLinks.map((nav, index) =>(
-        <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-dimWhite ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}>
-          <a href={`#${nav.id}`}>{nav.title}</a>
-        </li>
-      ))}</ul>
-    </nav>
-  )
-}
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] text-dimWhite ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            }`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
 
-export default Navbar
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => (!prev))}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
