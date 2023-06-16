1dbad64379cd106a7f56ff6fc10c337accb9b51b
hashanproleadsoft <hemachandra.hashan@gmail.com>
2023-06-16 14:03:56 +0530

    Intro component

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "../style";
+import { IntroCard } from "../components"
 
-const Intro = () => <section>
-    <div className="bg-black">Intro</div>
-</section>;
+const Intro = () => (
+  <section className={`flex md:flex-row flex-col`}>
+    <div
+      className={`flex-1 ${styles.flexCenter} flex-col xl:px-0 sm:px-16 px-6 mb-40 `}
+    >
+      <IntroCard/>
+    </div>
+    <div
+      className={`flex-1 flex ${styles.flexStart} md:my-0 my-10 relative mr`}
+    >
+      Intro
+    </div>
+  </section>
+);
 
 export default Intro;
