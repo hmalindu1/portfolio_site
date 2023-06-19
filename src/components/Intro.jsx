033419548b0edcf7b986f9f3652536e37359a421
hashanproleadsoft <hemachandra.hashan@gmail.com>
2023-06-19 12:19:48 +0530

    Update Intro.jsx
    Explained the code and refactored

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,20 +1,22 @@
-import React from "react";
 import styles from "../style";
 import { IntroCard } from "../components"
 
-const Intro = () => (
-  <section className={`flex md:flex-row flex-col`}>
-    <div
-      className={`flex-1 ${styles.flexCenter} flex-col xl:px-0 sm:px-16 px-6 mb-40 `}
-    >
-      <IntroCard/>
-    </div>
-    <div
-      className={`flex-1 flex ${styles.flexStart} md:my-0 my-10 relative mr`}
-    >
-      Intro
-    </div>
-  </section>
-);
+/**
+ * Renders an introduction section with a card and some text.
+ */
+const Intro = () => {
+  return (
+    <section className={`flex md:flex-row flex-col`}>
+      {/* Card container */}
+      <div className={`flex-1 ${styles.flexCenter} flex-col xl:px-0 sm:px-16 px-6 mb-40 `}>
+        <IntroCard/>
+      </div>
+      {/* Text container */}
+      <div className={`flex-1 flex ${styles.flexStart} md:my-0 my-10 relative mr`}>
+        Intro
+      </div>
+    </section>
+  );
+}
 
 export default Intro;
