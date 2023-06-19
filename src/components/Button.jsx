5e49253aa3c005469693388875e865d4e711296a
hashanproleadsoft <hemachandra.hashan@gmail.com>
2023-06-19 12:19:34 +0530

    Update Button.jsx
    Explained the code and refactored

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-
-const Button = ({styles}) => {
+/**
+ * Renders a button component with specified styles
+ * @param {Object} props - The props object containing styles
+ * @returns {JSX.Element} - The button component
+ */
+const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`text-black rounded-full font-poppins mt-5 font-semibold transition-all duration-500 bg-gradient-to-tl from-blue-500 via-purple-500 to-blue-500 bg-size-200 bg-pos-0 hover:bg-pos-100 py-2 px-4 transform hover:scale-110`}
+      // Use template literals for better readability
+      className={`
+        text-black 
+        rounded-full 
+        font-poppins 
+        mt-5 
+        font-semibold 
+        transition-all 
+        duration-500 
+        bg-gradient-to-tl 
+        from-blue-500 
+        via-purple-500 
+        to-blue-500 
+        bg-size-200 
+        bg-pos-0 
+        hover:bg-pos-100 
+        py-2 
+        px-4 
+        transform 
+        hover:scale-110
+      `}
     >
       Contact
     </button>
